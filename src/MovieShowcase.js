d05e2cbbec8595be9c65f534fbe57fad7a0716f9
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-23 16:00:28 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+  generateMovieCards = props => {
+    // map over your movieData array and return the correct
+    // const generateMovieCards = props.MovieCard
+    return movieData.map(movieData => (
+      <MovieCard
+        title={movieData.title}
+        genres={movieData.genres}
+        poster={movieData.poster}
+        IMDBRating={movieData.IMDBRating}
+      />
+    ));
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,17 +11,26 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
+    console.log( this.props.IMDBRating)
     // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating == null){
+      return <h4>no rating found</h4>
+      
+    }
+   else {
+     return <img src= {imgMapper[this.props.IMDBRating]}/>
+   }
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
+        <span /> 
+
+        {this.generateRatingElement()}
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
-        <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres"> {this.props.genres.join(', ')}</h5>
       </div>
     )
   }
