fc55bbc0b9fc699385c966e422ee1548eee4ef79
Theodore Kimana <kimana.theodore@gmail.com>
2019-05-30 14:11:49 -0500

    d

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+    // map over your movieData array and return the correct
+  };
 
   render() {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
+        {movieData.map(movie => {
+        return  <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster}/>
+        })};
+    
       </div>
-    )
+    );
   }
 }
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import zero from '../assets/stars/0-stars.png'
-import one from '../assets/stars/1-stars.png'
-import two from '../assets/stars/2-stars.png'
-import three from '../assets/stars/3-stars.png'
-import four from '../assets/stars/4-stars.png'
-import five from '../assets/stars/5-stars.png'
+import React, { Component } from "react";
+import zero from "../assets/stars/0-stars.png";
+import one from "../assets/stars/1-stars.png";
+import two from "../assets/stars/2-stars.png";
+import three from "../assets/stars/3-stars.png";
+import four from "../assets/stars/4-stars.png";
+import five from "../assets/stars/5-stars.png";
 
-const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+const imgMapper = { 0: zero, 1: one, 2: two, 3: three, 4: four, 5: five };
 
 export default class CardBack extends Component {
-
-  generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
-  }
+ 
+  //   // implement meeeee! See the readme for instructions
+  // };
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">
+        {this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement}
+        
+        {/* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">
+        {this.props.genres}</h5>
       </div>
-    )
+    );
   }
 }
